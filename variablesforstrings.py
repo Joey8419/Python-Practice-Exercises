7a0309044914c4d800f4371c88a1606f7ca365ad
Joey8419 <145053136+Joey8419@users.noreply.github.com>
2024-01-24 20:48:42 -0500

    added code

--- variablesforstrings.py
+++ variablesforstrings.py
@@ -1,3 +1,15 @@
 # Declare a variable called myName & assign to it a string that represents your full name
 myName = "Josette Nelson-Magruder"
 
+# Declare a variable, tittled message and assign "Hello World" to the variable; print message
+message = "Hello World"
+print(message)
+
+# Take input of students biodata in variables and print the input data
+# Student Bio Data:
+name = "Aisha Batool"
+age = "17"
+university_name = "Habib University"
+course_name = "Computational Thinking-1"
+
+print(name, age, university_name, course_name)
