0b02750aeb74419bb631fdd46081490e46dcd2fe
Joey8419 <145053136+Joey8419@users.noreply.github.com>
2024-01-28 20:19:12 -0500

    Added code

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -14,3 +14,131 @@ if user_city == "Karachi":
     print("Welcome to city of lights!")
 else:
     print(f"Welcome to {user_city} !")
+
+
+"""Write a program to take "gender" as an input from user. If the user is male, give
+the message: Good morning Sir. If the user is female, give the message: Good morning Ma'am"""
+
+user_gender = input("Are you male or female? ")
+
+if user_gender == "male":
+    print("Good Morning Sir")
+if user_gender == "female":
+    print("Good Morning Ma'am")
+else:
+    print("Good Morning")
+
+
+""" Write a program to take input max age and current age from user. If the current age
+is less than or equal to max age, show the message "You are welcome". """
+
+max_age = int(input("Enter the maximum age: "))
+current_age = int(input("Enter your current age: "))
+
+if current_age <= max_age:
+    print("You are welcome")
+else:
+    print("Sorry you are not welcome")
+
+
+"""Write a program to take input remaining fuel in car (in litres) from user. If the 
+current fuel is less than 0.25litres, show the message "Please refill the fuel in your car". """
+
+remaining_fuel = float(input("Enter the amount of fuel in litres left in your car: "))
+
+if remaining_fuel < 0.25:
+    print("Please refill the fuel in your car")
+else:
+    print("Check tank you might need gas soon")
+
+   
+    """ Write a program to take input the marks obtained in three subjects and total marks. 
+    Compute and show the resulting percentage on your page. Take percentage and compute grade as per following table:
+    Show the total marks, marks obtained, percentage, grade, and remarks like:
+    Marks Sheet
+    Total Marks: 300
+    Marks Obtained: 219
+    Percentage: 73%
+    Grade: B
+    Remarks: You need to improve"""
+
+# Get input for marks obtained in three subjects and total marks
+subject1_marks = int(input("Enter marks obtained in subject 1: "))
+subject2_marks = int(input("Enter marks obtained in subject 2: "))
+subject3_marks = int(input("Enter marks obtained in subject 3: ")) 
+total_marks = int(input("Enter total marks: "))  
+
+# Calculate total marks obtained
+marks_obtained = subject1_marks + subject2_marks +subject3_marks
+
+# Calculate the percentage
+percentage = (marks_obtained / total_marks) * 100
+
+# Determine grade based percentage
+if percentage >= 80:
+    grade = "A-one"
+    remarks = "Excellent"
+elif percentage >= 70:
+    grade = "A"
+    remarks = "Good"
+elif percentage >= 60:
+    grade = "B"
+    remarks = "You need to improve"
+else:
+    grade = "Fail"
+    remarks = "Sorry"
+
+print("\nMarks Sheet")
+print(f"Total Marks: {total_marks}")
+print(f"Marks Obtained: {marks_obtained}")
+print(f"Percentage: {percentage:.2f}%")
+print(f"Grade: {grade}")
+print(f"Remarks: {remarks}")
+
+
+"""Write a program to implement checkout process of a shopping cart system for an
+e-commerce website. Take input from users, the following:
+a) Name of item1
+b) Name of item2
+c) Price of item 1
+d) Price of item 2
+e) Ordered quantity of item 1
+f) Ordered Quantity of item 2
+g) Shipping charges
+Compute the total cost. If the total cost is above 2000 PKR, offer them 10%
+discount & show the receipt in your browser.
+Shopping Cart
+Price of T-Shirt is 1000
+Quantity of T-Shirt is 2
+Price of USB Flash Drive is 3
+Shipping Charges 250
+Total Cost of Your Order is 4350 PKR
+Discounted Price is 3915 PKR"""
+
+item1_name = input("Name of first item: ")
+item2_name = input("Name of secone item: ")
+price1 = float(input("What is the price of the first item: "))
+price2 = float(input("What is the price of the second item: "))
+quantity1 = int(input("What is the quantity of the first item: "))
+quantity2 = int(input("What is the quantity of the second item: "))
+shipping_charges = float(input("Shipping charges: "))
+
+total_cost = (price1 * quantity1) + (price2 * quantity2) + shipping_charges
+
+if total_cost >= 2000:
+    discount_percentage = 10
+    discount_amount = (discount_percentage / 100) * total_cost
+    discounted_price = total_cost - discount_amount
+
+print("\nShopping Cart")
+print(f"Price of {item1_name} is {price1} PKR")
+print(f"Price of {item2_name} is {price2} PKR")
+print(f"Quantity of {item1_name} is {quantity1}")
+print(f"Quantity of {item2_name} is {quantity2}")
+print(f"Shipping charges: {shipping_charges} PKR")
+print(f"Total Cost Of Your Order is {total_cost} PKR    ")
+
+# Display discounted price
+if discounted_price != total_cost:
+    print(f"Discounted Price is {discounted_price} PKR")
+
