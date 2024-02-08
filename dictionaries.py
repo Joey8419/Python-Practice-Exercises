27b0ee6d7c66d5704da9940aa901e9d9f43dc8f0
Joey8419 <145053136+Joey8419@users.noreply.github.com>
2024-02-07 22:11:31 -0500

    added code to dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+# Dictionaries = data structure that has key value pairs and are mutable
+# keys are unchangeable/ immutable; use curly brackets {}
+
+students = {"Joey": "Second grade", "Josh": "Fifth grade", "John": "Third grade", "Sasha": "Fifth grade"}
+
+# Get the value of the key in the dictionary by looking up the students name (key)
+# Curly brackets now become square brackets
+print(students["John"])
+print(students["Joey"])
+print(students["Josh"])
+print(students["Sasha"])
