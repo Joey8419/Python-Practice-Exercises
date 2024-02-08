ba8c2aa64e267c0edfa2af417f75bdba3dfd5c7d
Joey8419 <145053136+Joey8419@users.noreply.github.com>
2024-02-08 12:50:56 -0500

    added code to dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -9,3 +9,22 @@ print(students["John"])
 print(students["Joey"])
 print(students["Josh"])
 print(students["Sasha"])
+
+# When using a for loop to iterate over a dictionary, it iterates over all the keys
+for student in students:
+    print(student)
+
+# Print out both if you want keys and value pairs
+for student in students:
+    print(student, students[students], sep=", ") # Seperator is used here to make code more readable
+
+# None represents the absence of a value
+# LISTS OF DICTIONARIES
+students = [{"name": "Joey", "grade": "Second grade", "subject": "Math"},
+            {"name": "Josh", "grade": "Fifth grade", "subject": "History"}, 
+            {"name": "John", "grade": "Third grade", "subject": "Reading"}, 
+            {"name": "Sasha", "grade": "Fifth grade", "subject": "Art"}]
+
+for student in students:
+    print(student["name"], student["house"], student["subject"], sep=", " )
+
